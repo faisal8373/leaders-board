5b7dc484f4f6a0c9642c37770d2fe50c295226c5
faisal8373 <faisal205.vu@gmail.com>
2021-12-20 10:02:44 +0500

    Minor CSS updates & increase/decrease functionality

--- src/leaderBoard/Students.js
+++ src/leaderBoard/Students.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Students(props) {
-    return (
-        <div className='studentContainer'>
-            <div className='nameUni'>
-            <h2 className='studentName'>Student Name: {props.name}</h2>
-            
-            <p className='university'>University Name</p>
-            </div>
-            <div className='stuScore'>
-            <h2 className='score'>Score: {props.score}</h2>
-            </div>
-        </div>
-    )
+  const [stuScore, setStuScore] = useState(props.score);
+
+  function ctaIncrease() {
+    setStuScore(stuScore + 1);
+  }
+
+  function ctaDecrease() {
+    setStuScore(stuScore - 1);
+  }
+
+  return (
+    <div className="studentContainer">
+      <div className="nameUni">
+        <h2 className="studentName">Student Name: {props.name}</h2>
+        <button className="increase" onClick={ctaIncrease}>
+          +
+        </button>
+        <button className="decrease" onClick={ctaDecrease}>
+          -
+        </button>
+        <p className="university">
+          University Name
+          {stuScore >= 350 ? (
+            <span className="success">Success</span>
+          ) : stuScore <= 340 ? (
+            <span className="fail">Fail</span>
+          ) : (
+            ""
+          )}
+        </p>
+      </div>
+      <div className="stuScore">
+        <h2 className="score">Score: {stuScore}</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Students
+export default Students;
